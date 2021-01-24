2377b7efa5d891ca807d86497bff2cb021ab8917
zhanganjin <zhanganjin0520@gmail.com>
2021-01-24 13:36:46 +0800

    rename the package name

--- lol_web/lol_admin/urls.py
+++ lol_web/lol_admin/urls.py
@@ -1,4 +1,4 @@
-"""lol_web URL Configuration
+"""lol_admin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- lol_web/lol_admin/wsgi.py
+++ lol_web/lol_admin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lol_web project.
+WSGI config for lol_admin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lol_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lol_admin.settings')
 
 application = get_wsgi_application()
